0099bf8640bee9c70227d033ade65f2053d34ab5
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-03-28 14:06:24 -0300

    Atualizações de estudo

--- JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Condicoes - SWITCH/Conversor de Medidas - Ex/index.js
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Condicoes - SWITCH/Conversor de Medidas - Ex/index.js
@@ -11,22 +11,22 @@ const unidadeMedida = prompt(
 
 switch (unidadeMedida) {
     case "1":
-        alert("O valor em milímetro(mm) é: " + valorMetros * 1000)
+        alert("O resultado é:  " + valorMetros * 1000 + "mm")
         break
     case "2":
-        alert("O valor em centímetro(cm) é: " + valorMetros * 100)
+        alert("O resultado é:  " + valorMetros * 100 + "cm")
         break
     case "3":
-        alert("O valor em decímetro(dm) é: " + valorMetros * 10)
+        alert("O resultado é:  " + valorMetros * 10 + "dm")
         break
     case "4":
-        alert("O valor em decâmetro(dam) é: " + valorMetros / 10)
+        alert("O resultado é:  " + valorMetros / 10 + "dam")
         break
     case "5":
-        alert("O valor em hectômetro(hm) é: " + valorMetros / 100)
+        alert("O resultado é:  " + valorMetros / 100 + "hm")
         break
     case "6":
-        alert("O valor em quilômetro(km) é: " + valorMetros / 1000)
+        alert("O resultado é:  " + valorMetros / 1000 + "km")
         break
     default:
         alert("Opção inválida")
