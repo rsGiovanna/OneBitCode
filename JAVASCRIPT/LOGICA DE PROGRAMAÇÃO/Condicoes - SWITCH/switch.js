91bc670467ae99378ecc57f90d96ace5a7bcf9dd
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-03-28 13:11:05 -0300

    Atualizações de estudo

---
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Condicoes - SWITCH/switch.js
@@ -0,0 +1,11 @@
+switch ("b") {
+    case "a":
+        alert("O resultado é 'a'")
+    case "b":
+        alert("O resultado é 'b'")
+        break
+    case "c":
+        alert("O resultado é 'c'")
+    default:
+        alert("Finalizando...")
+}
