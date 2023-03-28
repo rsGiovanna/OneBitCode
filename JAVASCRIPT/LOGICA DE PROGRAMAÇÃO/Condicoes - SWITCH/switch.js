c35c4bf2b47aaf56ee0f8190bc9c72f868f48f59
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-03-28 13:14:07 -0300

    Atualizações de estudo

--- JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Condicoes - SWITCH/switch.js
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Condicoes - SWITCH/switch.js
@@ -1,11 +1,15 @@
-switch ("b") {
+const resultado = prompt("Escolha uma alternativa: \na) \nb) \nc)")
+
+switch (resultado) {
     case "a":
         alert("O resultado é 'a'")
+        break
     case "b":
         alert("O resultado é 'b'")
         break
     case "c":
         alert("O resultado é 'c'")
+        break
     default:
         alert("Finalizando...")
 }
