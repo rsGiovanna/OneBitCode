3bfc92413c1a9540ffd21f5f886f92c16fc86d15
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-04-26 20:50:30 -0300

    Atualização de Estudos

---
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Estruturas de Dados/Array/index.js
@@ -0,0 +1,9 @@
+const listaDeCompras = ["Açúcar", false]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = 'Arroz'
+listaDeCompras[1] = 'Feijão'
+listaDeCompras[2] = 7
+listaDeCompras[5] = "Batata"
+console.log(listaDeCompras)
+console.log(listaDeCompras[10])
