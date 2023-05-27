62e34c737fc4be374788f3e5c7bdaf4279e6edb2
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-05-27 10:57:53 -0300

    Atualizações de Estudos

---
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Estruturas de Dados/Array Bidimensional/index.js
@@ -0,0 +1,10 @@
+const arr = [
+    "1° Nível",
+    ["2° Nível", 42, true],
+    [
+        ["3° Nível, 1° Item", "Olá, mundo!"],
+        ["3° Nível, 2° Item", "Oi, mundo!"],
+    ],
+    []
+]
+console.log(arr)
