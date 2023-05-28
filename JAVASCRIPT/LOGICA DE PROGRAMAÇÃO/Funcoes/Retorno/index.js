a2adb80ff0d016a530ee516183ffe803d884e60b
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-05-27 21:30:08 -0300

    Atualização de Estudos

---
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes/Retorno/index.js
@@ -0,0 +1,15 @@
+function calcularMedia(a, b){
+    const media = (a + b) / 2
+    return media
+}
+
+const resultado = calcularMedia(7, 2)
+
+console.log(resultado)
+
+function criarProduto(nome, preco){
+    const produto ={
+        nome,
+        preco
+    }
+}
