89dfb403a4996a2c71645cca4a9ecaccb3d4b116
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-05-28 10:52:18 -0300

    Atualização de Estudos

---
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes/Calculadora Geometrica - Ex/index.js
@@ -0,0 +1,13 @@
+const menu = ""
+
+do{
+    menu = prompt("Calcular:\n" + "1 - Área do Triângulo\n" + "2 - Área do Retângulo\n" + "3 - Área do Quadrado\n" + "4 - Área do Trapézio\n" + "5 - Área do Círculo\n" + "6 - Sair");
+
+    switch(menu){
+        case "1":
+            function areaTriangulo = prompt(base, altura) {
+                alert("O dobro de " + x + " é " + (x * 2));
+            }
+    }
+
+} while(menu != 6) 
---
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes/Escopo/index.js
@@ -0,0 +1,24 @@
+/* let pokemon = "Charmander"
+
+function evoluir() {
+    pokemon = "Charmeleon"
+}
+
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+
+function criarAnimal() {
+    let animal = "Gato"
+}
+
+criarAnimal()
+console.log(animal) */
+
+console.log(nome)
+console.log(sobrenome)
+
+var nome = "João"
+let sobrenome = "Silva"
+
+console.log(nome, sobrenome)
