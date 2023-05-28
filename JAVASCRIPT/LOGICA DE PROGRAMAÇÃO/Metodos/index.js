efb12fa74dd8fcec95f737ed329778c670925bb6
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-05-28 18:00:14 -0300

    Atualização de Estudos

--- JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Metodos/index.js
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Metodos/index.js
@@ -2,7 +2,12 @@ let pessoa = {
     nome: "João",
     idade: 29,
     dizerOla() {
-        console.log("Olá, mundo! Meu nome é " + this.nome + " e tenho " + this.idade + " anos.");
+        console.log(
+            "Olá, mundo! Meu nome é " +
+            this.nome +
+            " e tenho " +
+            this.idade +
+            " anos.");
     }
 }
 
