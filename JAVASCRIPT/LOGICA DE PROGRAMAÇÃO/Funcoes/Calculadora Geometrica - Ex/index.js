5c30411ff669126f44610315ae9623c6c4a23bff
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-05-28 17:39:52 -0300

    Atualização de Estudos

--- JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes/Calculadora Geometrica - Ex/index.js
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes/Calculadora Geometrica - Ex/index.js
@@ -1,13 +1,73 @@
-const menu = ""
+function calcularAreaTriangulo() {
+    const base = prompt("Informe a base do triângulo: ")
+    const altura = prompt("Informe a altura do triângulo: ")
+    return base * altura / 2
+}
+function calcularAreaRetangulo() {
+    const base = prompt("Informe a base do retângulo: ")
+    const altura = prompt("Informe a altura do retângulo: ")
+    return base * altura
+}
+function calcularAreaQuadrado() {
+    const lado = prompt("Informe a lado do quadrado: ")
+    return lado * lado
+}
+function calcularAreaTrapezio() {
+    const baseMaior = parseFloat(prompt("Informe a base maior do trapezio: "))
+    const baseMenor = parseFloat(prompt("Informe a base menor do trapezio: "))
+    const altura = prompt("Informe a altura do trapezio: ")
+    return (baseMaior + baseMenor) * altura / 2
+}
+function calcularAreaCirculo() {
+    const raio = prompt("Informe o raio do círculo: ")
+    return (3, 14 * raio * raio)
+}
 
-do{
-    menu = prompt("Calcular:\n" + "1 - Área do Triângulo\n" + "2 - Área do Retângulo\n" + "3 - Área do Quadrado\n" + "4 - Área do Trapézio\n" + "5 - Área do Círculo\n" + "6 - Sair");
+function exibirMenu() {
+    return prompt(
+        "Calculadora Geometrica:\n" +
+        "1 - Calcular área do Triângulo\n" +
+        "2 - Calcular área do Retângulo\n" +
+        "3 - Calcular área do Quadrado\n" +
+        "4 - Calcular área do Trapézio\n" +
+        "5 - Calcular área do Círculo\n" +
+        "6 - Sair"
+    );
+}
 
-    switch(menu){
-        case "1":
-            function areaTriangulo = prompt(base, altura) {
-                alert("O dobro de " + x + " é " + (x * 2));
-            }
-    }
+function executar() {
+    let opcao = ""
+    do {
+        opcao = exibirMenu()
+        let resultado
 
-} while(menu != 6) 
+        switch (opcao) {
+            case "1":
+                resultado = calcularAreaTriangulo()
+                break
+            case "2":
+                resultado = calcularAreaRetangulo()
+                break
+            case "3":
+                resultado = calcularAreaQuadrado()
+                break
+            case "4":
+                resultado = calcularAreaTrapezio()
+                break
+            case "5":
+                resultado = calcularAreaCirculo()
+                break
+            case "6":
+                alert("Saindo...")
+                break
+            default:
+                alert("Opção inválida!")
+        }
+
+        if (resultado){
+            alert("Resultado: " + resultado)
+        }
+    } while (opcao != "6")
+}
+
+executar()
