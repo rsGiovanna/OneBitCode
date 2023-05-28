5da905140558441c9240d296903a5b88d3f5b33d
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-05-27 21:42:50 -0300

    Atualização de Estudos

--- JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes/Retorno/index.js
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes/Retorno/index.js
@@ -1,14 +1,14 @@
-function calcularMedia(a, b){
+function calcularMedia(a, b) {
     const media = (a + b) / 2
     return media
 }
 
 const resultado = calcularMedia(7, 2)
 
-console.log(resultado)
+//console.log(resultado)
 
-function criarProduto(nome, preco){
-    const produto ={
+function criarProduto(nome, preco) {
+    const produto = {
         nome,
         preco,
         estoque: 1
@@ -17,4 +17,32 @@ function criarProduto(nome, preco){
 }
 
 
-console.log(criarProduto("Notebook Intel Core i3 8GB", 2500))
+//console.log(criarProduto("Notebook Intel Core i3 8GB", 2500))
+
+function areaRetangular(base, altura) {
+    return base * altura
+}
+function areaQuadrada(lado) {
+    return areaRetangular(lado, lado)
+}
+/* console.log(areaRetangular(3, 5))
+console.log(areaQuadrada(9)) */
+
+function ola() {
+    let texto = "..."
+    return texto
+    texto = "Olá, mundo!"
+    console.log(texto)
+}
+console.log(ola())
+
+function maioridade(idade) {
+    if (idade >= 18) {
+        return "Maior de idade"
+    } else {
+        return "Menor de idade"
+    }
+}
+
+console.log(maioridade(29))
+console.log(maioridade(13))
