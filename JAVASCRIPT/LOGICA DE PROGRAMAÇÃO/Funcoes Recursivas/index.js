3de0071118d417eaa30ea7dd2e07c0d308d2e29a
Giovanna Rodrigues <giovanna.fac67@gmail.com>
2023-05-28 21:35:33 -0300

    Atualização de Estudos

--- JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes Recursivas/index.js
+++ JAVASCRIPT/LOGICA DE PROGRAMAÇÃO/Funcoes Recursivas/index.js
@@ -1,10 +1,33 @@
-function dividir(num){
+function dividir(num) {
     console.log(num)
-    if(num % 2 === 0){
+    if (num % 2 === 0) {
         dividir(num / 2)
     } else {
         return num
     }
 }
 
-dividir()
+//dividir()
+
+// Primeira -> Segunda -> Terceira -> Quarta
+
+function dobrar(num) {
+    console.log(num)
+    dobrar(num * 2)
+}
+//dobrar(1)
+
+//fatorial de 5: 5! = 5 * 4 * 3 * 2 * 1 = 120
+function fatorial(num) {
+    console.log("Número: " + num)
+    if (num === 0) { // caso base
+        return 1
+    } else if (num === 1) {
+        return 1
+    } else {
+        console.log(num + " * !" + (num - 1))
+        return num * fatorial(num - 1)
+    }
+}
+
+console.log(fatorial(5))
